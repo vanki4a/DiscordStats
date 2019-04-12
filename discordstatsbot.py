c7151c01787f0ce3ec5326bd2300df4f7cd4add0
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-04-12 18:09:20 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -120,7 +120,7 @@ async def setnick(ctx, user: discord.Member, *, nickname):
 async def ban(ctx,user:discord.Member):
 
     if user.server_permissions.ban_members:
-        await client.say('**:x: I can't ban this user.**')
+        await client.say('**:x: I can`t ban this user.**')
         return
 
     try:
