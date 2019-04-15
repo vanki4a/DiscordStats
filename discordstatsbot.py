1bec701fa81d85a6843949e83a329fc093b957c5
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-04-15 15:37:27 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -215,7 +215,7 @@ async def clear(ctx, number):
        
     try:
         await client.delete_messages(mgs)          
-        await client.say(``str(number)+' messages deleted``')
+        await client.say(str(number)+' messages deleted.')
      
     except discord.Forbidden:
         await client.say(embed=Forbidden)
