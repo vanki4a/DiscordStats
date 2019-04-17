90757b13db505c984699e2169a27c97a4e66c724
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-04-17 18:16:34 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -267,9 +267,4 @@ async def backup(ctx, *, msg = None):
     await client.delete_message(ctx.message)
     await client.say('``Successfully backed up your server!``'
 	
-@client.command()
-async def invite(ctx, *, msg = None):
-	await client.say(':gift:')
-	await client.say('https://discordapp.com/api/oauth2/authorize?client_id=562959056357294100&permissions=8&scope=bot')
-  
 client.run(os.getenv('Token'))
