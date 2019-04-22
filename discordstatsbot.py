1d2549b635b1c58dcb449606e4b87ae4bca9a4e4
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-04-22 19:15:12 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -281,46 +281,11 @@ async def kick(ctx,user:discord.Member):
         return	
 
 @client.command(pass_context = True)
-async def play(ctx, *, url):
-    author = ctx.message.author
-    voice_channel = author.voice_channel
-    try:
-        vc = await client.join_voice_channel(voice_channel)
-        msg = await client.say("Loading...")
-        player = await vc.create_ytdl_player("**Youtube Search**" + url)
-        player.start()
-        await client.say("Succesfully Loaded ur song!")
-        await client.delete_message(msg)
-    except Exception as e:
-        print(e)
-        await client.say("**Reconnecting...**")
-        for x in client.voice_clients:
-            if(x.server == ctx.message.server):
-                await x.disconnect()
-                nvc = await client.join_voice_channel(voice_channel)
-                msg = await client.say("Loading...")
-                player2 = await nvc.create_ytdl_player("ytsearch:" + url)
-                player2.start()
-
-@client.command(pass_context = True)
 async def dmall(ctx, *, msg: str):
     for server_member in ctx.message.server.members:
       await client.send_message(server_member, msg)
       await client.delete_message(ctx.message)
-		
-@client.command(pass_context = True)
-async def stop(ctx):
-    for x in client.voice_clients:
-        if(x.server == ctx.message.server):
-            return await x.disconnect()
-
-@client.command(pass_context=True)
-@commands.has_permissions(kick_members=True)
-async def joinvoice(ctx):
-    author = ctx.message.author
-    channel = author.voice_channel
-    await client.join_voice_channel(channel)
-
+	
 @client.command()
 async def invite():
        await client.say('https://discordapp.com/api/oauth2/authorize?client_id=562959056357294100&permissions=8&scope=bot')
