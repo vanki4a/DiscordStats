f6a0e126688e312e8b187f8a78e7aa13ec991557
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-05-09 18:24:12 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -324,6 +324,16 @@ async def roles(context):
 async def square(number):
     squared_value = int(number) * int(number)
     await client.say(str(number) + " squared is " + str(squared_value))
+
+	
+@client.command()
+async def bitcoin():
+    url = 'https://api.coindesk.com/v1/bpi/currentprice/BTC.json'
+    async with aiohttp.ClientSession() as session:  # Async HTTP request
+        raw_response = await session.get(url)
+        response = await raw_response.text()
+        response = json.loads(response)
+        await client.say("Bitcoin price is: $" + response['bpi']['USD']['rate'])
 	
 @client.command()
 async def invite():
