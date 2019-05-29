17f700f363595d99cd2cfa7ae5a2bf51ae2e238c
vanki4a <48853075+vanki4a@users.noreply.github.com>
2019-05-29 13:29:38 +0300

    Update discordstatsbot.py

--- discordstatsbot.py
+++ discordstatsbot.py
@@ -300,7 +300,7 @@ async def kick(ctx,user:discord.Member):
 @commands.has_permissions(kick_members=True)
 async def getuser(ctx, role: discord.Role = None):
     if role is None:
-        await client.say('*There is no "STAFF" role on this server!*')
+        await client.say('**There is no "STAFF" role on this server!**')
         return
     empty = True
     for member in ctx.message.server.members:
